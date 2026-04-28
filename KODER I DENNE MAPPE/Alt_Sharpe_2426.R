e8bde9789eb8bdadb972934a1f4dfcbd73ed68a2
Victor Nielsen <vsni24@student.aau.dk>
2026-04-28 13:33:25 +0200

    afkast,variance, sharpe tabel

--- KODER I DENNE MAPPE/Alt_Sharpe_2426.R
+++ KODER I DENNE MAPPE/Alt_Sharpe_2426.R
@@ -73,3 +73,91 @@ tangency_portfolio_1 <- as.numeric((sigma_inv_1%*%(R_1-rf_1*u_1))/(A_1-rf_1*C_1)
 sharpe_ratio_1 <- as.numeric((tangency_portfolio%*%R_1 - rf_1)/sqrt((tangency_portfolio%*% sigma_1 %*% tangency_portfolio))) 
 
 ###### Hvis du køre denne sammen med Jacobs kode og får den gammle "tangency_portfolio" ude i din data og variable consol til højre kan den kode køres, og du vil få Sharpe ratio på 2.065386.
+
+assets_1 <- assets_1 |>
+  mutate(Return_2426 = mu, Variance_2426 = sd^2) |>
+  select(Return_2426, Variance_2426)
+
+assets <- assets |>
+  mutate(Return = mu, Variance = sd^2) |>
+  select(-mu,-sd)
+
+group_1 <- bind_cols(assets, assets_1)
+
+group_1 <- group_1 |>
+  mutate(Sharpe_2426 = (Return_2426 - rf_1) / sqrt(Variance_2426)) |>
+  select(symbol, x_MIN, z_star, tangency_portfolio, Return, Return_2426, Variance, Variance_2426, sharpe, Sharpe_2426)
+
+
+
+
+
+summaries %>% 
+  ggplot() +
+  geom_line(data = summary_efc, 
+            aes(x = sd*sqrt(252), y = mu*252), 
+            color = "steelblue") +
+  geom_line(data = summary_rf,
+            aes(x = sd*sqrt(252),
+                y = mu*252),
+            color = "darkgreen") +
+  geom_point(data = summary_min,
+             aes(x = sd*sqrt(252), y = mu*252),
+             color = "red",
+             size = 3) +
+  geom_point(data = assets, 
+             aes(x = sd*sqrt(252), y = mu*252)) +
+  geom_point(data = summary_rf,
+             aes(x=0, 
+                 y=rf*252), 
+                 color ="red", 
+                 size=3) +
+  geom_point(data = summary_tan,
+             aes(x = sd*sqrt(252),
+                 y = mu*252),
+             color = "red",
+             size = 3) +
+  geom_point(data = tibble(x = sqrt(Market_var*252), y = market_R*252),
+           aes(x = x, y = y),
+           color = "red",
+           size = 3) +
+  geom_label(data = tibble(
+             x = sqrt(Market_var * 252),
+             y = market_R * 252),
+           aes(x = x, y = y),
+           label = "bold(m)",
+           parse = TRUE,
+           vjust = -0.7) +
+          
+  geom_label(data = summary_tan,
+             aes(x = sd*sqrt(252),
+                 y = mu*252,
+             label = "bold(tilde(v))"),
+             parse = TRUE,
+             hjust = -0.5) +
+  geom_label(data = summary_rf,
+             aes(x = 0,
+                 y = rf*252,
+             label = "bold(tilde(x))[rf]^MIN"),
+             parse = TRUE,
+             hjust = -0.35) +
+  geom_label(
+    data = summary_min,
+    aes(x = sd*sqrt(252), 
+        y = mu*252, 
+        label = "bold(tilde(x))^MIN"),
+    parse = TRUE,
+    vjust = 1.5,
+    hjust = 0.5
+  ) +
+  geom_label(aes(x = 0.37, y = 0.28),
+           label = "Assets") +
+  scale_x_continuous(labels = scales::percent,
+                     limits = c(-0.05,0.7)) +
+  scale_y_continuous(labels = scales::percent,
+                     limits = c(-0.1, 1),
+                     breaks = breaks_width(0.1)) +
+  
+  labs(x = "Standard deviation", 
+       y = "Expected return") +
+  theme_minimal()
